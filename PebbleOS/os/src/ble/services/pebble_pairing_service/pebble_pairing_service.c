5404b66fe5f494cf8e2124a0e123db82a9722bbb
Evan Stoddard <evan.stoddard@me.com>
2026-03-05 16:30:43 -0600

    Bare-minimum of PPS implemented.

--- PebbleOS/os/src/ble/services/pebble_pairing_service/pebble_pairing_service.c
+++ PebbleOS/os/src/ble/services/pebble_pairing_service/pebble_pairing_service.c
@@ -21,6 +21,8 @@
 
 #include "ble/pebble_bt.h"
 
+#include "ble/services/pebble_pairing_service/pebble_pairing_service_private.h"
+
 /*****************************************************************************
  * Definitions
  *****************************************************************************/
@@ -44,6 +46,14 @@ LOG_MODULE_REGISTER(pebble_pairing_service);
 /*****************************************************************************
  * Variables
  *****************************************************************************/
+
+/**
+ * @brief Private instance data
+ */
+static struct {
+  const struct bt_gatt_attr *conn_status_attr;
+  const struct bt_gatt_attr *conn_params_attr;
+} prv_inst;
 
 /*****************************************************************************
  * BLE Binding Declarations
@@ -160,30 +170,175 @@ BT_GATT_SERVICE_DEFINE(
  * Private Functions
  *****************************************************************************/
 
+/**
+ * @brief Handle reception of remote param mgmt setings
+ *
+ * @param conn Pointer to connection
+ * @param settings Pointer to incoming settings
+ * @return Returns a BT_ATT_ERR value (BT_ATT_ERR_SUCCESS if no error)
+ */
+static uint8_t prv_handle_remote_param_settings(struct bt_conn *conn,
+                                                PebblePairingServiceConnParamsWrite_t *settings,
+                                                uint16_t buf_len) {
+  size_t num_param_sets = (buf_len - sizeof(PebblePairingServiceConnParamsWrite_t)) /
+                          sizeof(PebblePairingServiceConnParamSet_t);
+
+  LOG_INF(
+      "Received remote param settings mgmt cmd:\r\n"
+      "\tRemote managing: %u\r\n"
+      "\tNum param sets: %zu\r\n",
+      settings->remote_param_mgmt_settings.is_remote_device_managing_connection_parameters,
+      num_param_sets);
+
+  // TODO: Actually handle these settings...
+
+  return BT_ATT_ERR_SUCCESS;
+}
+
+/**
+ * @brief Populate connectivity status struct for given connection
+ *
+ * @param conn Pointer to connection
+ * @param status Pointer to struct to write info into
+ */
+static void prv_populate_conn_status_struct(struct bt_conn *conn,
+                                            PebblePairingServiceConnectivityStatus_t *status) {
+  if (conn == NULL || status == NULL) {
+    return;
+  }
+
+  struct bt_conn_info conn_info = {0};
+  bt_conn_get_info(conn, &conn_info);
+
+  memset(status, 0, sizeof(PebblePairingServiceConnectivityStatus_t));
+
+  status->ble_is_connected = true;
+  status->ble_is_bonded = bt_le_bond_exists(conn_info.id, conn_info.le.dst);
+  status->ble_is_encrypted = (conn_info.security.level >= BT_SECURITY_L2);
+  status->supports_pinning_without_security_request = true;
+  status->is_reversed_ppogatt_enabled = true;
+
+  LOG_INF(
+      "Fetched connection status: \r\n"
+      "\tConnected: %u\r\n"
+      "\tBonded: %u\r\n"
+      "\tEncrpyted: %u\r\n",
+      status->ble_is_connected, status->ble_is_bonded, status->ble_is_encrypted);
+}
+
+/**
+ * @brief Notify phone of changes to connection status
+ *
+ * @param conn Pointer to connection
+ */
+static void prv_notify_conn_status(struct bt_conn *conn) {
+  PebblePairingServiceConnectivityStatus_t status = {0};
+  prv_populate_conn_status_struct(conn, &status);
+
+  if (prv_inst.conn_status_attr == NULL) {
+    return;
+  }
+
+  int ret = bt_gatt_notify(conn, prv_inst.conn_status_attr, &status, sizeof(status));
+
+  if (ret < 0) {
+    LOG_ERR("Failed to notify connection status update: %d", ret);
+  }
+}
+
+/**
+ * @brief Handle a pairing trigger request and log its contents
+ *
+ * @param conn Pointer to connection
+ * @param buf Pointer to written data
+ * @param len Length of written data
+ * @return Returns a BT_ATT_ERR value (BT_ATT_ERR_SUCCESS if no error)
+ */
+static uint8_t prv_handle_pairing_trigger_request(struct bt_conn *conn, const void *buf,
+                                                  uint16_t len) {
+  const PairingTriggerRequestData_t *req = (const PairingTriggerRequestData_t *)buf;
+
+  LOG_INF(
+      "Pairing trigger request:\r\n"
+      "\tshould_pin_address: %u\r\n"
+      "\tno_slave_security_request: %u\r\n"
+      "\tshould_force_slave_security_request: %u\r\n"
+      "\tshould_auto_accept_re_pairing: %u\r\n"
+      "\tis_reversed_ppogatt_enabled: %u",
+      req->should_pin_address, req->no_slave_security_request,
+      req->should_force_slave_security_request, req->should_auto_accept_re_pairing,
+      req->is_reversed_ppogatt_enabled);
+
+  struct bt_conn_info conn_info = {0};
+  bt_conn_get_info(conn, &conn_info);
+
+  // Set security level to require encryption if needed
+  if ((!req->no_slave_security_request && conn_info.security.level < BT_SECURITY_L2) ||
+      req->should_force_slave_security_request) {
+    int ret = bt_conn_set_security(conn, BT_SECURITY_L2);
+
+    if (ret < 0) {
+      LOG_ERR("Failed to update security level: %d", ret);
+    }
+
+    prv_notify_conn_status(conn);
+  }
+
+  return BT_ATT_ERR_SUCCESS;
+}
+
 /*****************************************************************************
  * BLE Binding Defintins
  *****************************************************************************/
 
 static ssize_t prv_on_read_conn_status(struct bt_conn *conn, const struct bt_gatt_attr *attr,
                                        void *buf, uint16_t len, uint16_t offset) {
-  LOG_INF("Phone requesting connection status.");
+  PebblePairingServiceConnectivityStatus_t status = {0};
 
-  return BT_GATT_ERR(BT_ATT_ERR_NOT_SUPPORTED);
+  prv_populate_conn_status_struct(conn, &status);
+
+  return bt_gatt_attr_read(conn, attr, buf, len, offset, &status, sizeof(status));
 }
 
 static ssize_t prv_on_read_trigger_pairing(struct bt_conn *conn, const struct bt_gatt_attr *attr,
                                            void *buf, uint16_t len, uint16_t offset) {
-  LOG_INF("Phone requestion trigger pairing data?");
+  if (len < 1) {
+    return BT_GATT_ERR(BT_ATT_ERR_INVALID_ATTRIBUTE_LEN);
+  }
 
-  return BT_GATT_ERR(BT_ATT_ERR_NOT_SUPPORTED);
+  LOG_HEXDUMP_INF(buf, len, "Trigger Pairing Payload:");
+
+  uint8_t pairing_flags = ((uint8_t *)buf)[0];
+
+  struct bt_conn_info conn_info = {0};
+  bt_conn_get_info(conn, &conn_info);
+
+  bool no_sec_req = (pairing_flags & 0x02) != 0;
+  bool force_sec_req = (pairing_flags & 0x04) != 0;
+
+  // Set security level to require encryption if needed
+  if ((!no_sec_req && conn_info.security.level < BT_SECURITY_L2) || force_sec_req) {
+    int ret = bt_conn_set_security(conn, BT_SECURITY_L2);
+
+    if (ret < 0) {
+      LOG_ERR("Failed to update security level: %d", ret);
+    }
+
+    prv_notify_conn_status(conn);
+  }
+
+  return len;
 }
 
 static ssize_t prv_on_write_trigger_pairing(struct bt_conn *conn, const struct bt_gatt_attr *attr,
                                             const void *buf, uint16_t len, uint16_t offset,
                                             uint8_t flags) {
-  LOG_INF("Phone writing trigger pairing data?");
+  uint8_t ret = prv_handle_pairing_trigger_request(conn, buf, len);
+  if (ret != BT_ATT_ERR_SUCCESS) {
+    return BT_GATT_ERR(ret);
+  }
 
-  return BT_GATT_ERR(BT_ATT_ERR_NOT_SUPPORTED);
+  return len;
 }
 
 static ssize_t prv_on_read_conn_params(struct bt_conn *conn, const struct bt_gatt_attr *attr,
@@ -196,23 +351,76 @@ static ssize_t prv_on_read_conn_params(struct bt_conn *conn, const struct bt_gat
 static ssize_t prv_on_write_conn_params(struct bt_conn *conn, const struct bt_gatt_attr *attr,
                                         const void *buf, uint16_t len, uint16_t offset,
                                         uint8_t flags) {
-  LOG_INF("Phone writing connection params?");
+  PebblePairingServiceConnParamsWrite_t *conn_params_req =
+      (PebblePairingServiceConnParamsWrite_t *)buf;
 
-  return BT_GATT_ERR(BT_ATT_ERR_NOT_SUPPORTED);
+  uint8_t ret = BT_ATT_ERR_SUCCESS;
+
+  switch (conn_params_req->cmd) {
+    case PebblePairingServiceConnParamsWriteCmd_SetRemoteParamMgmtSettings:
+      ret = prv_handle_remote_param_settings(conn, conn_params_req, len);
+      break;
+    default:
+      LOG_INF("Unhandled write conn params cmd: 0x%02X", conn_params_req->cmd);
+      ret = BT_ATT_ERR_NOT_SUPPORTED;
+      break;
+  }
+
+  return BT_GATT_ERR(ret);
 }
 
 static void prv_on_conn_status_ccc_changed(const struct bt_gatt_attr *attr, uint16_t value) {
-  LOG_INF("Connection status characteristic config changed.");
+  if (value & BT_GATT_CCC_NOTIFY) {
+    prv_inst.conn_status_attr = attr - 1;
+  } else {
+    prv_inst.conn_status_attr = NULL;
+  }
 }
 
 static void prv_on_conn_params_ccc_changed(const struct bt_gatt_attr *attr, uint16_t value) {
-  LOG_INF("Connection params characteristic config changed.");
+  if (value & BT_GATT_CCC_NOTIFY) {
+    prv_inst.conn_params_attr = attr - 1;
+  } else {
+    prv_inst.conn_params_attr = NULL;
+  }
 }
 
+/**
+ * @brief Callback from BLE stack, indicating a disconnection
+ *
+ * @param conn Pointer to connection
+ * @param reason Disconnect reason
+ */
+static void prv_on_disconnected(struct bt_conn *conn, uint8_t reason) {
+  prv_inst.conn_status_attr = NULL;
+  prv_inst.conn_params_attr = NULL;
+}
+
+/**
+ * @brief Callback from BLE stack, indicating security level changed
+ *
+ * @param conn Pointer to connection
+ * @param level Level changed to
+ * @param err Any error indication
+ */
+static void prv_on_security_changed(struct bt_conn *conn, bt_security_t level,
+                                    enum bt_security_err err) {
+  if (err) {
+    LOG_ERR("Security change failed: level %d, err %d", level, err);
+  } else {
+    LOG_INF("Security changed: level %d", level);
+    prv_notify_conn_status(conn);
+  }
+}
+
+BT_CONN_CB_DEFINE(prv_conn_callbacks) = {
+    .disconnected = prv_on_disconnected,
+    .security_changed = prv_on_security_changed,
+};
 /*****************************************************************************
  * Functions
  *****************************************************************************/
 
 int pebble_pairing_service_init(void) {
-  return -ENOTSUP;
+  return 0;
 }
