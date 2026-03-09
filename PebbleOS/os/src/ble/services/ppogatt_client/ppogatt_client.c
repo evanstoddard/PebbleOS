b88be18c7df2e62a2f3715368c6df29eeaee0902
Evan Stoddard <evan.stoddard@me.com>
2026-03-09 12:30:58 -0500

    Fetch PPoGATT meta on service discovery.

--- PebbleOS/os/src/ble/services/ppogatt_client/ppogatt_client.c
+++ PebbleOS/os/src/ble/services/ppogatt_client/ppogatt_client.c
@@ -17,8 +17,15 @@
 #include <zephyr/bluetooth/gatt.h>
 #include <zephyr/bluetooth/uuid.h>
 
+#include <zephyr/sys/slist.h>
+
 #include "ble/ble_conn_mgr.h"
 #include "ble/pebble_bt.h"
+
+#include "kernel/kernel_heap.h"
+
+#include "ppogatt_client_internal.h"
+#include "zephyr/sys/uuid.h"
 
 /*****************************************************************************
  * Definitions
@@ -55,16 +62,120 @@ static const struct bt_uuid *prv_ppogatt_char_uuids[PPOGATT_CHAR_COUNT] = {
     [PPOGATT_CHAR_DATA] = (const struct bt_uuid *)&prv_ppogatt_data_uuid,
     [PPOGATT_CHAR_META] = (const struct bt_uuid *)&prv_ppogatt_meta_uuid,
 };
+
 /**
  * @brief Private instance
  */
 static struct {
   struct bt_conn *conn;
+
+  sys_slist_t clients;
+
 } prv_inst;
 
 /*****************************************************************************
  * Private Functions
  *****************************************************************************/
+
+/**
+ * @brief Instantiate and initialize new PPoGATT client
+ *
+ * @return Returns pointer to new client
+ */
+PPoGATT_Client_t *prv_create_client(void) {
+  PPoGATT_Client_t *client = kernel_heap_malloc(sizeof(PPoGATT_Client_t));
+
+  if (client == NULL) {
+    return NULL;
+  }
+
+  memset(client, 0, sizeof(PPoGATT_Client_t));
+
+  return client;
+}
+
+/**
+ * @brief [TODO:description]
+ *
+ * @param conn [TODO:parameter]
+ * @param err [TODO:parameter]
+ * @param params [TODO:parameter]
+ * @param data [TODO:parameter]
+ * @param len [TODO:parameter]
+ * @return [TODO:return]
+ */
+static uint8_t prv_on_meta_read(struct bt_conn *conn, uint8_t err,
+                                struct bt_gatt_read_params *params, const void *data,
+                                uint16_t len) {
+  if (err != 0) {
+    LOG_ERR("Failed to read meta data: %u", err);
+    // FIXME: Need a way to signal that this PPoGATT client needs to be destroyed
+    return BT_GATT_ITER_STOP;
+  }
+
+  PPoGATT_Client_t *client = CONTAINER_OF(params, PPoGATT_Client_t, read_params);
+
+  PPoGATTMetaV1_t *meta = (PPoGATTMetaV1_t *)data;
+
+  if (len < sizeof(PPoGATTMetaV0_t)) {
+    LOG_ERR("Invalid meta size.");
+    // FIXME: Need a way to signal that this PPoGATT client needs to be destroyed
+    return BT_GATT_ITER_STOP;
+  }
+
+  uint8_t meta_version = 0;
+
+  client->meta.ppogatt_min_version = meta->ppogatt_min_version;
+  client->meta.ppogatt_max_version = meta->ppogatt_max_version;
+  client->meta.app_uuid = meta->app_uuid;
+
+  if (len >= sizeof(PPoGATTMetaV1_t)) {
+    meta_version++;
+    if (meta->pp_session_type >= PPoGATTSessionTypeCount) {
+      LOG_ERR("Invalid session type.");
+      // FIXME: Need a way to signal that this PPoGATT client needs to be destroyed
+      return BT_GATT_ITER_STOP;
+    }
+
+    client->meta.pp_session_type = meta->pp_session_type;
+  }
+
+  char uuid_str[UUID_STR_LEN] = {0};
+  uuid_to_string(&meta->app_uuid, uuid_str);
+
+  LOG_INF(
+      "Fetch Meta:\r\n"
+      "\tMeta Version: %u\r\n"
+      "\tMin Version: %u\r\n"
+      "\tMax Version: %u\r\n"
+      "\tUUID: %s\r\n"
+      "\tSession Type: %u",
+      meta_version, client->meta.ppogatt_min_version, client->meta.ppogatt_max_version, uuid_str,
+      client->meta.pp_session_type);
+
+  return BT_GATT_ITER_STOP;
+}
+
+/**
+ * @brief Fetch meta from server
+ *
+ * @param client Pointer to client instance
+ * @return Returns 0 on success
+ */
+int prv_fetch_meta(PPoGATT_Client_t *client) {
+  client->read_params.handle_count = 1;
+  client->read_params.single.offset = 0;
+  client->read_params.single.handle = client->meta_handle;
+  client->read_params.func = prv_on_meta_read;
+
+  int ret = bt_gatt_read(prv_inst.conn, &client->read_params);
+
+  if (ret < 0) {
+    LOG_ERR("Failed to queue meta read: %d", ret);
+  }
+
+  return ret;
+}
 
 /*****************************************************************************
  * BLE Bindings
@@ -78,6 +189,31 @@ static struct {
  */
 static void prv_on_service_discovered(uint16_t *handles, uint16_t service_end_handle) {
   LOG_INF("PPoGATT service discovered!");
+
+  PPoGATT_Client_t *client = prv_create_client();
+
+  if (client == NULL) {
+    // TODO: Fatal?
+    LOG_ERR("Unable to allocate new PPoGATT client.");
+
+    return;
+  }
+
+  client->meta_handle = handles[PPOGATT_CHAR_META];
+  client->data_handle = handles[PPOGATT_CHAR_DATA];
+  client->service_end_handle = service_end_handle;
+
+  int ret = prv_fetch_meta(client);
+  if (ret != 0) {
+    LOG_ERR("Failed to fetch client meta data: %d", ret);
+
+    // FIXME: Deallocate client
+    return;
+  }
+
+  // FIXME: Guard with mutex. Original code had overarching BT lock.  Unsure if that is best
+  // decision...
+  sys_slist_append(&prv_inst.clients, (sys_snode_t *)client);
 }
 
 /**
---
+++ PebbleOS/os/src/ble/services/ppogatt_client/ppogatt_client_internal.h
@@ -0,0 +1,131 @@
+/*
+ * Copyright (C) Evan Stoddard
+ */
+
+/**
+ * @file ppogatt_client_internal.h
+ * @author Evan Stoddard
+ * @brief
+ */
+
+#ifndef ppogatt_client_internal_h
+#define ppogatt_client_internal_h
+
+#include <stdint.h>
+
+#include <zephyr/bluetooth/gatt.h>
+
+#include <zephyr/sys/slist.h>
+#include <zephyr/sys/uuid.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*****************************************************************************
+ * Definitions
+ *****************************************************************************/
+#define PPOGATT_V1_DESIRED_RX_WINDOW_SIZE (4500 / 251)
+#define PPOGATT_MIN_VERSION (0x00)
+#define PPOGATT_MAX_VERSION (0x01)
+#define PPOGATT_SN_BITS (5)
+#define PPOGATT_SN_MOD_DIV (1 << PPOGATT_SN_BITS)
+#define PPOGATT_V0_WINDOW_SIZE (4)
+#define PPOGATT_TIMEOUT_TICK_INTERVAL_SECS (2)
+
+//! Effective timeout: between 5 - 6 secs, because packet could be sent out just before the
+//! RegularTimer second tick is about to fire.
+#define PPOGATT_TIMEOUT_TICKS (3)
+
+//! Number of maximum consecutive timeouts without getting a packet Ack'd
+#define PPOGATT_TIMEOUT_COUNT_MAX (2)
+
+//! Number of maximum consecutive resets without getting a packet Ack'd
+#define PPOGATT_RESET_COUNT_MAX (5)
+
+//! Number of maximum consecutive disconnects without getting a packet Ack'd
+#define PPOGATT_DISCONNECT_COUNT_MAX (2)
+
+//! Maximum amount of time PPoGATT will wait before sending an Ack for received data
+#define PPOGATT_MAX_DATA_ACK_LATENCY_MS (200)
+
+/*****************************************************************************
+ * Structs, Unions, Enums, & Typedefs
+ *****************************************************************************/
+
+typedef enum {
+  PPoGATTPacketTypeData = 0x0,
+  PPoGATTPacketTypeAck = 0x1,
+  PPoGATTPacketTypeResetRequest = 0x2,
+  PPoGATTPacketTypeResetComplete = 0x3,
+  PPoGATTPacketTypeInvalidRangeStart,
+} PPoGATTPacketType_t;
+
+typedef struct PPoGATTPacket_t {
+  PPoGATTPacketType_t type : 3;
+  uint8_t sn : PPOGATT_SN_BITS;
+  uint8_t payload[];
+} __attribute__((__packed__)) PPoGATTPacket_t;
+
+//! Client identification payload that is attached to the client's Reset Request messages
+typedef struct PPoGATTResetRequestClientIDPayload_t {
+  //! The PPoGATT version that the client wants to use.
+  //! Must be within the server's [ppogatt_min_version, ppogatt_max_version]
+  uint8_t ppogatt_version;
+
+  //! The serial number of the client device.
+  char serial_number[CONFIG_MFG_SERIAL_NUMBER_SIZE];
+} __attribute__((__packed__)) PPoGATTResetRequestClientIDPayload_t;
+
+typedef struct PPoGATTResetCompleteClientIDPayloadV1_t {
+  uint8_t ppogatt_max_rx_window;
+  uint8_t ppogatt_max_tx_window;
+} __attribute__((__packed__)) PPoGATTResetCompleteClientIDPayloadV1_t;
+
+typedef struct PPoGATTMetaV0_t {
+  uint8_t ppogatt_min_version;
+  uint8_t ppogatt_max_version;
+  struct uuid app_uuid;
+} __attribute__((__packed__)) PPoGATTMetaV0_t;
+
+typedef enum PPoGATTSessionType_t {
+  PPoGATTSessionType_InferredFromUuid = 0x00,
+  PPoGATTSessionType_Hybrid = 0x01,
+  PPoGATTSessionTypeCount,
+} __attribute__((__packed__)) PPoGATTSessionType_t;
+
+typedef struct PPoGATTMetaV1_t {
+  uint8_t ppogatt_min_version;
+  uint8_t ppogatt_max_version;
+  struct uuid app_uuid;
+  PPoGATTSessionType_t pp_session_type : 8;
+} __attribute__((__packed__)) PPoGATTMetaV1_t;
+
+/**
+ * @typedef PPoGATT_Client_t
+ * @brief Instance of a PPoGATT client
+ *
+ */
+typedef struct PPoGATT_Client_t {
+  sys_snode_t node;
+
+  uint16_t meta_handle;
+  uint16_t data_handle;
+  uint16_t service_end_handle;
+
+  struct bt_gatt_read_params read_params;
+  struct bt_gatt_subscribe_params subscribe_params;
+  struct bt_gatt_discover_params ccc_disc_params;
+
+  PPoGATTMetaV1_t meta;
+
+} PPoGATT_Client_t;
+
+/*****************************************************************************
+ * Function Prototypes
+ *****************************************************************************/
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* ppogatt_client_internal_h */
